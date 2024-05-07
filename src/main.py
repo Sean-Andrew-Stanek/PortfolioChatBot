3a7c16577940d03753c8cf8e06be5cbcd61f9887
Sean Andrew Stanek <146111111+Sean-Andrew-Stanek@users.noreply.github.com>
2024-05-07 10:42:55 +0900

    Imp[lements API_KEY switches

--- src/main.py
+++ src/main.py
@@ -1,10 +1,25 @@
 from flask import Flask, request, jsonify;
+import os
 
-app = Flask(__name__);
+# Attempt to get the API key from the environment
+API_KEY = os.getenv('API_KEY')
+
+# If not env, try to get it locally.  If that fails, it throws an error and halts the code.
+if not API_KEY:
+    try:
+        from chat_gpt_api_key import API_KEY
+    except ImportError:
+        raise RuntimeError('API_KEY not found.  Either set your environment variable or create a file called chat_gpt_api_key.py and set your API_KEY there.')
+
+
+app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return '';
+    return ''
+
+
+
 
 ############
 #  Return  #
@@ -12,11 +27,11 @@ def home():
 
 @app.route('/get-next-line')
 def getNextLine():
-    return '';
+    return ''
 
 ##################
 #  Server Start  #
 ##################
 
 if __name__ == '__main__':
-    app.run(debug=True);
+    app.run(debug=True)
