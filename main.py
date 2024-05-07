4d6e6254121f9d10fdb12ec62e1185f8d9f44536
Sean Andrew Stanek <146111111+Sean-Andrew-Stanek@users.noreply.github.com>
2024-05-07 12:50:26 +0900

    attempt to get response - failed

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from flask import Flask, request, jsonify;
 import os
+import config
+import openai
 
 
+#############
+#  API KEY  #
+#############
 
 # Attempt to get the API key from the environment
 API_KEY = os.getenv('API_KEY')
@@ -13,12 +18,26 @@ if not API_KEY:
     except ImportError:
         raise RuntimeError('API_KEY not found.  Either set your environment variable or create a file called chat_gpt_api_key.py and set your API_KEY there.')
 
+openai.api_key = API_KEY
 
 app = Flask(__name__)
 
+messages = [{'role': 'system', 'content': config.system_role}]
+
+#############
+#  Routes  #
+#############
+
 @app.route('/')
 def home():
-    return ''
+    messages.append({'role': 'user', 'content': 'What do you think about the weather?'})
+    response = openai.ChatCompletion.create(
+        model = 'gpt-3.5-turbo',
+        messages = messages
+    )
+    reply = response['choices'][0]['message']['content']
+    messages.append({'role':'assistant', 'conent': reply})
+    return reply
 
 
 
