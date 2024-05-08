e63e311654ebd5f4465dd3f879683a434cb3dc92
lazyjinn1 <joshjbarba@gmail.com>
2024-05-08 13:50:46 -0700

    Added functionalities
    Added a new HTML page for front-end interaction
    
    Added a basic JS Form and which connects to the '/' route and fetches a POST method to '/chat'

---
+++ config.py
@@ -0,0 +1,1 @@
+system_role = 'This is a simulated chat between two people. Each response is the next line in the chat.'
--- main.py
+++ main.py
@@ -1,9 +1,13 @@
-from flask import Flask, request, jsonify;
+from flask import Flask, render_template, request, jsonify;
 import os
+import config
+from openai import OpenAI
 
 
+#############
+#  API KEY  #
+#############
 
-# Attempt to get the API key from the environment
 API_KEY = os.getenv('API_KEY')
 
 # If not env, try to get it locally.  If that fails, it throws an error and halts the code.
@@ -13,13 +17,46 @@ if not API_KEY:
     except ImportError:
         raise RuntimeError('API_KEY not found.  Either set your environment variable or create a file called chat_gpt_api_key.py and set your API_KEY there.')
 
+client = OpenAI(
+    api_key=API_KEY
+)
 
 app = Flask(__name__)
 
+messages = [{
+    'role': 'system', 
+    'content': config.system_role
+}]
+
+
+#############
+#  Routes  #
+#############
+
+
+
 @app.route('/')
 def home():
-    return ''
+    return render_template('chat.html')
 
+@app.route('/chat', methods=['POST'])
+def chat():
+    data = request.json
+    print(data)
+    user_message = data['message']
+
+    messages.append({'role': 'user', 'content': user_message}) #user message is added onto messages dictionary
+
+    response = client.chat.completions.create( #creating the AI message
+        model='gpt-3.5-turbo', #ai model used
+        messages=messages, #this allows ai to use ALL the messages sent so far
+        max_tokens=50 
+    )
+    ai_reply = response.choices[-1].message.content #defining the latest ai message
+    messages.append({'role': 'system', 'content': ai_reply}) #ai message is added onto messages
+
+
+    return jsonify({'reply': ai_reply}) #printing latest ai message
 
 
 
