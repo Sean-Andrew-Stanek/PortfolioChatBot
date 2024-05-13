6f0fd3eaf83e6da3fc900545b8b3e9d158b2a4e9
lazyjinn1 <joshjbarba@gmail.com>
2024-05-13 12:24:55 -0700

    Removed Uncondensed Jeriko Projects

--- config.py
+++ config.py
@@ -11,18 +11,6 @@ Jeriko_fanboi = {'role': 'assistant', 'content': 'You are a huge fan of the web 
 ###################
 # System Roles #
 ###################
-# Token Count: 466
-Jeriko_Carrera_Projects = {'role': 'system', 'content': '''Jeriflix (React) is a movie application where users can create accounts and access movie information. Utilizing React, SCSS, JavaScript, and React-Bootstrap, it connects to its own database. The application is hosted on Netlify. The username and password for access are both 'KingdomCome'. ' \
-'Jeriflix (Angular) is an updated version of the Jeriflix movie application, using Angular as its framework while maintaining the same backend. It offers similar functionalities to Jeriflix (React) with additional features provided by Angular-Material.' \
-'Chat is an anonymous chat-room application built with React-Native, CSS, and JavaScript. It integrates Google\'s OAuth and Firebase for authentication and real-time database functionality. '\
-'Meet is an events application designed to connect with Google\'s Calendar API. Developed using React, CSS, JavaScript, and hosted on GitHub Pages, it offers tools for event creation and management. '\
-'Pokedex is a web application created with raw JavaScript, HTML, and CSS, connecting to the PokeAPI to provide information about Pokémon. It\'s a simple and intuitive way to learn about various Pokémon species. '\
-'Online Autho is an authorization form application built using React-Native and Firestore/Firebase. It enables organizations to create customizable authorization forms. '\
-'Portfolio is a showcase of projects created by the developer. Utilizing React, SCSS, and JavaScript, it offers insights into the developer\'s work and progress. '\
-'CLI Recipe is a command-line interface application developed in Python. It uses MySQL databases and an ORM for data management. '\
-'Movie API is a custom API constructed for the Jeriflix websites. It\'s built with Express and Node.js, facilitating interactions with MongoDB. '\
-'Recipe App is a Django-based web application allowing users to log in and explore or add recipes. Developed with Python and SQL, it offers a seamless recipe management experience.'''
-}
 # Token Count: 196
 Jeriko_Carrera_Projects_Condensed = {'role': 'system', 'content': '''Jeriko's projects - Jeriflix (React): Movie app for account creation and info access using React, SCSS, JavaScript, React-Bootstrap; connects to database, hosted on Netlify. 
 Jeriflix (Angular): Updated Jeriflix with Angular, same backend, includes Angular-Material.
