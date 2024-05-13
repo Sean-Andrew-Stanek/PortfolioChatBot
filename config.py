f0d344697358a577b1e1e06ad28151e7af9e2bd2
lazyjinn1 <joshjbarba@gmail.com>
2024-05-13 12:38:10 -0700

    Update config.py

--- config.py
+++ config.py
@@ -23,10 +23,7 @@ CLI Recipe: CLI app in Python using MySQL, ORM for data management.
 Movie API: Custom API for Jeriflix using Express, Node.js, MongoDB.
 Recipe App: Django web app for recipe management, uses Python, SQL.'''}
 
-system_role = {'role': 'system', 'content': 'This is a simulated chat between a recruiter or developer visiting the portfolio site of Jeriko Carrera/Sean Stanek. ' \
-'Jeriko Carrera has worked on JavaScript, HTML, CSS, Angular, Python, React and TypeScript. Sean Stanek has worked with JavaScript, HTML, CSS, Angular, Python ' \
-'React, TypeScript C# and more. The system should be hyping up Jeriko or Sean and should ask the user which one they are asking about first. ' \
-'Jeriko Carrera has worked on a Pokemon Application (JS), 2 movie applications (React and Angular), an events app (React), a mobile chat app (react-native) and more'}
+system_role = {'role': 'system', 'content': 'This is a simulated chat between a recruiter or developer visiting the portfolio site of Jeriko Carrera/Sean Stanek. '}
 
 example_dev = {'role': 'system', 'content': 'frontend Angular Angular Material Bootstrap CSS HTML JSX JWT JavaScript Node.js React React Bootstrap React Native RxJS SCSS SPA TypeScript ' \
 'Vite oAuth backend APIs AWS Lambda Express.js Firebase JWT MongoDB Mongoose Node.js PWA Passport.js Serverless bcrypt other AWS S3 Expo GIMP Gherkin Git ' \
